16f89e02b07c5cc95198d028ff3421554759f525
IlyaChistogov <115698573+IlyaChistogov@users.noreply.github.com>
2022-12-28 00:16:16 +0500

    Находим сколько раз используется слово Москва в 9 столбце

--- Lab4.py
+++ Lab4.py
@@ -5,3 +5,6 @@ with open("1.csv","r") as file:
 	keys = file.readline()
 	reader = csv.reader(file, delimiter=",")
 	dataset = [x for x in reader]
+	keys = list(keys.split(","))
+	town = list(map(lambda x: x[9] == 'Москва', dataset))
+	print(town.count(True))
